08d67c13d174383641bc9ca1198ddab731ac139f
palakjain03 <palak2004jain@gmail.com>
2026-01-05 09:29:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,4 +8,9 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", auth);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
