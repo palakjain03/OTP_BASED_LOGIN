90829d1fa38b84d14ea8ec5e7385d3af6ecb94b5
palakjain03 <palak2004jain@gmail.com>
2026-01-05 09:44:27 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 
 const mysql = require("mysql2");
-module.exports = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "otp_login"
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+
+module.exports = db;
